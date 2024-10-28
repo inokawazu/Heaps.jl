fea73beeca66fa15db011ffa31722708f6cfe4e4
Markus Amano <markus.a.amano@gmail.com>
2024-10-28 12:45:53 +0900

    cleaned up code comments

--- src/Heaps.jl
+++ src/Heaps.jl
@@ -2,11 +2,6 @@ module Heaps
 
 export heapify, minheap, maxheap, Heap
 
-# 0
-# 1 2
-# 3 4 5 6
-# 7 8 9 10 11 12 13 14
-
 struct Heap{T}
     elements::Vector
     lt::Function
@@ -57,30 +52,6 @@ end
 function Base.push!(h::Heap, elem, rest...)
     push!(push!(h, elem), rest...)
 end
-
-# 1
-# 2 3
-# 4 5
-#
-# _
-# 2 3
-# 4 5
-#
-# 5
-# 2 3
-# 4
-#
-# 2
-# 4 3
-# 5
-
-# 1
-# 3 2
-# 4 5
-#
-# 5
-# 3 2
-# 4
 
 Base.isempty(h::Heap) = isempty(h.elements)
 
